b22bbeccc1ca7167e6070b433b2b315ecd8131d3
luiscarm9 <lc.rivera10@uniandes.edu.co>
2016-02-21 16:26:17 -0500

    lab

--- lab3_hybrid/vis_hybrid_image.m
+++ lab3_hybrid/vis_hybrid_image.m
@@ -3,7 +3,7 @@ function output = vis_hybrid_image(hybrid_image)
 %concatenating all of the images together.
 
 scales = 5; %how many downsampled versions to create
-scale_factor = 0.5; %how much to downsample each time
+scale_factor = 0.7; %how much to downsample each time
 padding = 5; %how many pixels to pad.
 
 original_height = size(hybrid_image,1);
@@ -14,12 +14,12 @@ cur_image = hybrid_image;
 for i = 2:scales
     %add padding
     output = cat(2, output, ones(original_height, padding, num_colors));
-    
+
     %dowsample image;
     cur_image = imresize(cur_image, scale_factor, 'bilinear');
     %pad the top and append to the output
     tmp = cat(1,ones(original_height - size(cur_image,1), size(cur_image,2), num_colors), cur_image);
-    output = cat(2, output, tmp);    
+    output = cat(2, output, tmp);
 end
 
 
